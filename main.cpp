ec1bcda992c664fe8228de300179137177a7b727
Niwenjin <ni_wenjin@qq.com>
2023-10-27 16:03:58 +0800

    add timer

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include "ThreadPool.h"
+#include <chrono>
 #include <iostream>
 
 int main() {
+    auto start_time = std::chrono::high_resolution_clock::now();
+
     ThreadPool *pool = new ThreadPool();
     pool->thread_run();
     delete (pool);
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time);
+    std::cout << "Execution time: " << duration.count() << " seconds."
+              << std::endl;
+
     return 0;
 }
